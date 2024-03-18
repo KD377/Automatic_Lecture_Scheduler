594ee96ac79200b63dbe1d91946dd81d17d33f89
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2024-03-18 11:01:06 +0100

    Starting project

---
+++ lecture-scheduler/src/main/java/com/example/lecturescheduler/LectureSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecturescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureSchedulerApplication.class, args);
+	}
+
+}
---
+++ lecture-scheduler/src/test/java/com/example/lecturescheduler/LectureSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
