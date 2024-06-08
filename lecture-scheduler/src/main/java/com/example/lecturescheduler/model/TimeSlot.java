6824e43c86ca6e85a0e76558e71aab7bcb143c8a
242484 Olejniczak Jakub <242484@edu.p.lodz.pl>
2024-06-09 00:12:28 +0200

    Added LectureSessionRepository logic for finding lecture sessions by email od instructor

---
+++ lecture-scheduler/src/main/java/com/example/lecturescheduler/model/TimeSlot.java
@@ -0,0 +1,20 @@
+package com.example.lecturescheduler.model;
+
+import jakarta.persistence.Embeddable;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter
+@Getter
+@Builder
+@Embeddable
+public class TimeSlot implements Serializable {
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+}
