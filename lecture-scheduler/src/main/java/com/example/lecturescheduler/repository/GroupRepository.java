9cfe72d6c808e0e743fabd88ab7c19e3168d5a79
hare32 <234694@edu.p.lodz.pl>
2024-06-14 11:19:09 +0200

    GroupControllerIntegrationTest

--- lecture-scheduler/src/main/java/com/example/lecturescheduler/repository/GroupRepository.java
+++ lecture-scheduler/src/main/java/com/example/lecturescheduler/repository/GroupRepository.java
@@ -3,5 +3,8 @@ package com.example.lecturescheduler.repository;
 import com.example.lecturescheduler.model.SingleGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GroupRepository extends JpaRepository<SingleGroup, Long> {
+    Optional<SingleGroup> findByName(String name);
 }
