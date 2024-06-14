0f36be400620d96887486c6c223db950cba73742
hare32 <234694@edu.p.lodz.pl>
2024-06-14 11:41:10 +0200

    Instructor and Subject Integration Tests

--- lecture-scheduler/src/main/java/com/example/lecturescheduler/repository/InstructorRepository.java
+++ lecture-scheduler/src/main/java/com/example/lecturescheduler/repository/InstructorRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
     @Query("SELECT i FROM Instructor i JOIN i.subjectsTaught s WHERE s = :subject")
     List<Instructor> findBySubject(@Param("subject") Subject subject);
+    Optional<Instructor> findByEmail(String mail);
 }
--- lecture-scheduler/src/main/java/com/example/lecturescheduler/repository/SubjectRepository.java
+++ lecture-scheduler/src/main/java/com/example/lecturescheduler/repository/SubjectRepository.java
@@ -3,5 +3,8 @@ package com.example.lecturescheduler.repository;
 import com.example.lecturescheduler.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findByName(String newSubject);
 }
